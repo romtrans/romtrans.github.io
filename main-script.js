a58f5adbb8d386db384e8404cc8d0f84e112c12f
romtrans <romtrans.dev@gmail.com>
2025-01-31 22:23:53 +0100

    Created My home page template with described 3 project including recent one

---
+++ main-script.js
@@ -0,0 +1,39 @@
+document.querySelectorAll('.crt86').forEach(div => {
+    const bg = div.getAttribute('data-background'); // Pobiera nazwę pliku
+    if (bg) { 
+        div.style.backgroundImage = `url('${bg}')`;
+        console.log(`url do tła ('${bg}')`);
+    }
+});
+
+// Funkcja do wyświetlania szczegółów projektu
+function showDetails(projectName, file) {
+    // Ukrywamy główną sekcję z projektami
+    document.querySelector('.container-main').classList.add('hidden');
+    
+    // Pokazujemy szczegóły w container-details
+    const containerDetails = document.querySelector('.container-details');
+    containerDetails.style.display = 'block';
+
+    // Wstawiamy odpowiednią zawartość do container-details
+    let content = '';
+    if (projectName && file) {
+        content = `
+            <h2>Details about:</h2>
+            <iframe src="./projects/${file}" title="${projectName}" width="100%" height="80%" style="border: none; border-radius: 10px;"></iframe>
+            <button class="back-button" onclick="hideDetails()">Go back</button>
+        `;
+    }
+
+    // Wstawiamy zawartość do container-details
+    containerDetails.innerHTML = content;
+}
+
+// Funkcja do ukrywania szczegółów i przywracania widoku projektów
+function hideDetails() {
+    // Ukrywamy sekcję z detalami projektu
+    document.querySelector('.container-details').style.display = 'none';
+
+    // Pokazujemy ponownie sekcję z projektami
+    document.querySelector('.container-main').classList.remove('hidden');
+}
